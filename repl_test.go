79126e6d93ee4271e9ee306f3fc4fdd909374dbb
flogit2161 <flo12gab@gmail.com>
2025-12-30 01:57:11 +0100

    TestCase cleanInput

---
+++ repl_test.go
@@ -0,0 +1,40 @@
+package main 
+
+import "testing"
+
+func TestCleanInput(t *testing.T){
+	cases := []struct{
+		input string
+		expected []string
+	}{
+		{
+			input:     " hello    world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:"hello WOrLd",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:"hellO wOlrD",
+			expected: []string{"hello", "world"},
+		},
+		
+	}
+
+	for _, c := range cases{
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected){
+			t.Errorf("Test Failed, length of slice %d, expected length %d", len(actual), len(c.expected))
+			continue
+		}
+		
+		for i := range actual{
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord{
+				t.Errorf("Test Failed, word mismatch, actual: %v, expected: %v", word, expectedWord)
+			}
+		}
+	}
+}
